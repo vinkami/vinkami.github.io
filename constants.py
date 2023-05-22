7c0fc8766e7e884816371e7a8a5816aa31d8eb95
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2023-05-22 23:19:23 +0800

    Separated constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+URLS = {
+    # static file: template file
+    "index.html": "index.html",
+    "tool/avcombiner.html": "tool/avcombiner.html",
+}
+
+RENDER_ARGS = {
+    "discord": "https://discord.gg/vinkami",
+    "youtube": "https://youtube.com/@vinkamiii",
+    "twitter": "https://twitter.com/vinkamiii",
+    "twitch": "https://twitch.tv/vinkami",
+    "github": "https://github.com/vinkami"
+}
